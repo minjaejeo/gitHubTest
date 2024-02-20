d17d5c3cabcb31c8643ddb7c48b0356d4be629f3
NCP08 <navercloudplatform08.gmail.com>
2024-02-20 10:25:48 +0900

    Hello Puppy 추가

--- _01_FirstJavaProject/src/first01/hello01/HelloWorld.java
+++ _01_FirstJavaProject/src/first01/hello01/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("HelloWorld");
 		System.out.println("HelloJava");
 		System.out.println("Hello EveryOne");
+		System.out.println("Hello Puppy");
 	}
 
 }
